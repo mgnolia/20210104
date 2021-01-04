b5919cd54190c9c8da62e6d0bb0f4c1e75f6ca4f
mgnolia <mgnolia@users.noreply.github.com>
2021-01-04 14:53:06 +0900

    210100

---
+++ Test210104/src/Test_1.java
@@ -0,0 +1,17 @@
+
+public class Test_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		       short num1 = 22;
+		       short num2 = 11;
+		       double result = num1+num2;
+		       System.out.println(result);
+		  }
+		
+		
+
+	}
+
+
---
+++ Test210104/src/Test_2.java
@@ -0,0 +1,12 @@
+
+public class Test_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short num1 = 3;
+	       short num2 = 5;
+	       System.out.println(num1+num2+ '=' +(num1+num2));
+
+	}
+
+}
